3dc7d02640e79a24a661013dff4edb41611f1ac6
wileyquinnb <117231083+wileyquinnb@users.noreply.github.com>
2023-04-07 16:41:09 -0700

    +onLoad +sectionScroller *styles *html

---
+++ js/onLoad.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+    const container = document.getElementById("container");
+    const section0 = document.getElementById("section0");
+    const yOffset = (window.innerHeight / 1.23);
+    container.scrollTo({ top: yOffset, behavior: "auto" });
+});
