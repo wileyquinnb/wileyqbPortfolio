182e7049c63d2214cdc997506f6ba5e752772b38
wileyquinnb <117231083+wileyquinnb@users.noreply.github.com>
2023-04-10 13:38:35 -0700

    *implemented scroller mapping

--- js/scroller.js
+++ js/scroller.js
@@ -145,24 +145,24 @@ async function loadProjectScroller(event) {
     const response = await fetch(manifestUrl);
     const projects = await response.json();
 
-    let newContent = '';
-    for (let i = 0; i < projects.length; i++) {
-        const firstOrLast = i === 0 ? 'firstBox' : (i === projects.length - 1 ? 'lastBox' : '');
-        newContent += `
-            <div class="box fadeIn ${firstOrLast}" style="z-index: 100;">
-                <img src="./images/${secondaryParentFolder}/${projects[i]}">
-            </div>
-        `;
-    }
-
-    // const newContent = projects.map((project, i) => {
+    // let newContent = '';
+    // for (let i = 0; i < projects.length; i++) {
     //     const firstOrLast = i === 0 ? 'firstBox' : (i === projects.length - 1 ? 'lastBox' : '');
-    //     return `
-    //       <div class="box fadeIn ${firstOrLast}" style="z-index: 100;">
-    //         <img src="./images/${secondaryParentFolder}/${project}">
-    //       </div>
+    //     newContent += `
+    //         <div class="box fadeIn ${firstOrLast}" style="z-index: 100;">
+    //             <img src="./images/${secondaryParentFolder}/${projects[i]}">
+    //         </div>
     //     `;
-    // }).join()
+    // }
+
+    const newContent = projects.map((project, i) => {
+        const firstOrLast = i === 0 ? 'firstBox' : (i === projects.length - 1 ? 'lastBox' : '');
+        return `
+          <div class="box fadeIn ${firstOrLast}" style="z-index: 100;">
+            <img src="./images/${secondaryParentFolder}/${project}">
+          </div>
+        `;
+    }).join('');
 
     const scrollerDiv = visibleSection.querySelector('.scroller');
     scrollerDiv.classList.remove('slideRight');
@@ -342,6 +342,11 @@ function collapseSection(visibleSection) {
         boxDiv.classList.remove('enlargeBox');
     }
 
+    for (const scroller of scrollers) {
+        scroller.classList.remove('scroller100');
+        scroller.classList.add('scroller106');
+    }
+
     setTimeout(() => {
         visibleSection.classList.add('sectionShrink');
     }, 100);
@@ -356,10 +361,10 @@ function collapseSection(visibleSection) {
         visibleSection.classList.remove('sectionGrow');
         visibleSection.classList.remove('sectionShrink');
         titleDiv.innerHTML = originalTitleContent[visibleSection.id];
+        cardTitle.textContent = '';
+        cardText.textContent = '';
+        cardTitle.classList.remove('cardTitle');
+        cardText.classList.remove('cardText');
     }, 420);
-
-    for (const scroller of scrollers) {
-        scroller.classList.remove('scroller100');
-        scroller.classList.add('scroller106');
-    }
+    //Lol
 }
